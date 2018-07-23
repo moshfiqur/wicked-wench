cea780dca3432ca805e9d517c9a027833f995f5c
Moshfiqur Rahman <rahman.moshfiqur@gmail.com>
2018-07-23 17:24:12 +0200

    added script to extract color (unfinished)

---
+++ ssd_mobilenet_v2_coco_2018_03_29/extract_color.py
@@ -0,0 +1,75 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+
+
+try:
+    npz = np.load('../data/color_names.npz')
+    try:
+        # return npz[key]
+        print(npz['samples'][1])
+        print(npz['labels'][1])
+    except KeyError as e:
+        stderr.write('Failed to load {} matrix: `{}`\n'.format(e))
+        exit(1)
+except Exception as e:
+    stderr.write('Failed to load npz archive: `{}`\n'.format(e))
+    exit(1)
+
+quit()
+
+
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image")
+args = vars(ap.parse_args())
+
+# load the image
+image = cv2.imread(args["image"])
+
+# define the list of boundaries
+boundaries = [
+	([17, 15, 100], [50, 56, 200]),
+	([86, 31, 4], [220, 88, 50]),
+	([25, 146, 190], [62, 174, 250]),
+	([103, 86, 65], [145, 133, 128])
+]
+
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image")
+args = vars(ap.parse_args())
+
+# load the image
+image = cv2.imread(args["image"])
+
+# define the list of boundaries
+boundaries = [
+	([17, 15, 100], [50, 56, 200]),
+	([86, 31, 4], [220, 88, 50]),
+	([25, 146, 190], [62, 174, 250]),
+	([103, 86, 65], [145, 133, 128])
+]
+
+# loop over the boundaries
+for (lower, upper) in boundaries:
+	# create NumPy arrays from the boundaries
+	lower = np.array(lower, dtype = "uint8")
+	upper = np.array(upper, dtype = "uint8")
+
+	# find the colors within the specified boundaries and apply
+	# the mask
+	mask = cv2.inRange(image, lower, upper)
+	output = cv2.bitwise_and(image, image, mask = mask)
+
+	# show the images
+	cv2.imshow("images", np.hstack([image, output]))
+	cv2.waitKey(0)
